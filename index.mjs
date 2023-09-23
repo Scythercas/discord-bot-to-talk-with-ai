f7c7a27b0ef5192fea8010b6643bc91c391b63d3
Scythercas <111866216+Scythercas@users.noreply.github.com>
2023-09-23 17:18:08 +0900

    Update index.mjs
    文字数制限を緩くした
    256->512

--- index.mjs
+++ index.mjs
@@ -50,7 +50,7 @@ async function requestChatAPI(text, index) {
 
   const payload = {
     model: "gpt-3.5-turbo",
-    max_tokens: 256, //文字数制限
+    max_tokens: 512, //文字数制限
     messages: messagesSettings,
   };
   const response = await axios.post(
